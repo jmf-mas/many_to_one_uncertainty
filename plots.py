a7f1b7d332ac1d9cb17da974d2f18513142ad2a3
jmf-mas <masakunafelicien@gmail.com>
2023-09-24 01:01:40 -0400

    update

--- plots.py
+++ plots.py
@@ -157,8 +157,145 @@ def get_plots(i, metric, filename):
     print("scc ids", scc(y_train_ids, y_pred_train_ids)[0], scc(y_val_ids, y_pred_val_ids)[0], scc(y_test_ids, y_pred_test_ids)[0])
     print("scc kitsune", scc(y_train_kitsune, y_pred_train_kitsune)[0], scc(y_val_kitsune, y_pred_val_kitsune)[0], scc(y_test_kitsune, y_pred_test_kitsune)[0])
     print("scc ciciot", scc(y_train_ciciot, y_pred_train_ciciot)[0], scc(y_val_ciciot, y_pred_val_ciciot)[0], scc(y_test_ciciot, y_pred_test_ciciot)[0])
-    
 
+def get_lines(i, metric, filename):
+    
+    y_pred_train_ids = np.loadtxt(directory_outputs + ids + "_pred_train_"+metric+"_"+str(i)+".csv", delimiter=",")
+    y_pred_val_ids = np.loadtxt(directory_outputs + ids + "_pred_val_"+metric+"_"+str(i)+".csv", delimiter=",")
+    y_pred_test_ids = np.loadtxt(directory_outputs + ids + "_pred_test_"+metric+"_"+str(i)+".csv", delimiter=",")
+    
+    y_pred_train_nsl = np.loadtxt(directory_outputs + nsl + "_pred_train_"+metric+"_"+str(i)+".csv", delimiter=",")
+    y_pred_val_nsl = np.loadtxt(directory_outputs + nsl + "_pred_val_"+metric+"_"+str(i)+".csv", delimiter=",")
+    y_pred_test_nsl = np.loadtxt(directory_outputs + nsl +"_pred_test_"+metric+"_"+str(i)+".csv", delimiter=",")
+    
+    y_pred_train_kdd = np.loadtxt(directory_outputs + kdd + "_pred_train_"+metric+"_"+str(i)+".csv", delimiter=",")
+    y_pred_val_kdd = np.loadtxt(directory_outputs + kdd +"_pred_val_"+metric+"_"+str(i)+".csv", delimiter=",")
+    y_pred_test_kdd = np.loadtxt(directory_outputs + kdd +"_pred_test_"+metric+"_"+str(i)+".csv", delimiter=",")
+    
+    y_pred_train_kitsune = np.loadtxt(directory_outputs + kitsune + "_pred_train_"+metric+"_"+str(i)+".csv", delimiter=",")
+    y_pred_val_kitsune = np.loadtxt(directory_outputs + kitsune +"_pred_val_"+metric+"_"+str(i)+".csv", delimiter=",")
+    y_pred_test_kitsune = np.loadtxt(directory_outputs + kitsune +"_pred_test_"+metric+"_"+str(i)+".csv", delimiter=",")
+    
+    y_pred_train_ciciot = np.loadtxt(directory_outputs + ciciot + "_pred_train_"+metric+"_"+str(i)+".csv", delimiter=",")
+    y_pred_val_ciciot = np.loadtxt(directory_outputs + ciciot +"_pred_val_"+metric+"_"+str(i)+".csv", delimiter=",")
+    y_pred_test_ciciot = np.loadtxt(directory_outputs + ciciot +"_pred_test_"+metric+"_"+str(i)+".csv", delimiter=",")
+    
+    
+    XY_kdd_train = np.loadtxt(directory_data + kdd + "_train_latent_" + str(i) +".csv", delimiter=',')
+    y_train_kdd = XY_kdd_train[:, metrics[metric]]
+    X_train_kdd = XY_kdd_train[:, :-2]
+    XY_kdd_val = np.loadtxt(directory_data + kdd + "_val_latent_" + str(i) +".csv", delimiter=',')
+    y_val_kdd = XY_kdd_val[:, metrics[metric]]
+    X_val_kdd = XY_kdd_val[:, :-2]
+    XY_kdd_test = np.loadtxt(directory_data + kdd +"_test_latent_" + str(i) +".csv", delimiter=',')
+    y_test_kdd = XY_kdd_test[:, metrics[metric]]
+    X_test_kdd = XY_kdd_test[:, :-2]
+        
+    XY_nsl_train = np.loadtxt(directory_data + nsl + "_train_latent_" + str(i) +".csv", delimiter=',')
+    y_train_nsl = XY_nsl_train[:, metrics[metric]]
+    X_train_nsl = XY_nsl_train[:, :-2]
+    XY_nsl_val = np.loadtxt(directory_data + nsl + "_val_latent_" + str(i) +".csv", delimiter=',')
+    y_val_nsl = XY_nsl_val[:, metrics[metric]]
+    X_val_nsl = XY_nsl_val[:, :-2]
+    XY_nsl_test = np.loadtxt(directory_data + nsl + "_test_latent_" + str(i) +".csv", delimiter=',')
+    y_test_nsl = XY_nsl_test[:, metrics[metric]]
+    X_test_nsl = XY_nsl_test[:, :-2]
+        
+    XY_ids_train = np.loadtxt(directory_data + ids + "_train_latent_" + str(i) +".csv", delimiter=',')
+    y_train_ids = XY_ids_train[:, metrics[metric]]
+    X_train_ids = XY_ids_train[:, :-2]
+    XY_ids_val = np.loadtxt(directory_data + ids + "_val_latent_" + str(i) +".csv", delimiter=',')
+    y_val_ids = XY_ids_val[:, metrics[metric]]
+    X_val_ids = XY_ids_val[:, :-2]
+    XY_ids_test = np.loadtxt(directory_data + ids + "_test_latent_" + str(i) +".csv", delimiter=',')
+    y_test_ids = XY_ids_test[:, metrics[metric]]
+    X_test_ids = XY_ids_test[:, :-2]
+    
+    XY_kitsune_train = np.loadtxt(directory_data + kitsune + "_train_latent_" + str(i) +".csv", delimiter=',')
+    y_train_kitsune = XY_kitsune_train[:, metrics[metric]]
+    X_train_kitsune = XY_kitsune_train[:, :-2]
+    XY_kitsune_val = np.loadtxt(directory_data + kitsune + "_val_latent_" + str(i) +".csv", delimiter=',')
+    y_val_kitsune = XY_kitsune_val[:, metrics[metric]]
+    X_val_kitsune = XY_kitsune_val[:, :-2]
+    XY_kitsune_test = np.loadtxt(directory_data + kitsune + "_test_latent_" + str(i) +".csv", delimiter=',')
+    y_test_kitsune = XY_kitsune_test[:, metrics[metric]]
+    X_test_kitsune = XY_kitsune_test[:, :-2]
+    
+    XY_ciciot_train = np.loadtxt(directory_data + ciciot + "_train_latent_" + str(i) +".csv", delimiter=',')
+    y_train_ciciot = XY_ciciot_train[:, metrics[metric]]
+    X_train_ciciot = XY_ciciot_train[:, :-2]
+    XY_ciciot_val = np.loadtxt(directory_data + ciciot + "_val_latent_" + str(i) +".csv", delimiter=',')
+    y_val_ciciot = XY_ciciot_val[:, metrics[metric]]
+    X_val_ciciot = XY_ciciot_val[:, :-2]
+    XY_ciciot_test = np.loadtxt(directory_data + ciciot + "_test_latent_" + str(i) +".csv", delimiter=',')
+    y_test_ciciot = XY_ciciot_test[:, metrics[metric]]
+    X_test_ciciot = XY_ciciot_test[:, :-2]
+    
+    yi_train_kdd = pd.DataFrame(data={'ensemble': y_train_kdd, 'prediction': y_pred_train_kdd})
+    yi_val_kdd = pd.DataFrame(data={'ensemble': y_val_kdd, 'prediction': y_pred_val_kdd})
+    yi_test_kdd = pd.DataFrame(data={'ensemble': y_test_kdd, 'prediction': y_pred_test_kdd})
+    
+    yi_train_nsl = pd.DataFrame(data={'ensemble': y_train_nsl, 'prediction': y_pred_train_nsl})
+    yi_val_nsl = pd.DataFrame(data={'ensemble': y_val_nsl, 'prediction': y_pred_val_nsl})
+    yi_test_nsl = pd.DataFrame(data={'ensemble': y_test_nsl, 'prediction': y_pred_test_nsl})
+    
+    yi_train_ids = pd.DataFrame(data={'ensemble': y_train_ids, 'prediction': y_pred_train_ids})
+    yi_val_ids = pd.DataFrame(data={'ensemble': y_val_ids, 'prediction': y_pred_val_ids})
+    yi_test_ids = pd.DataFrame(data={'ensemble': y_test_ids, 'prediction': y_pred_test_ids})
+    
+    yi_train_kitsune = pd.DataFrame(data={'ensemble': y_train_kitsune, 'prediction': y_pred_train_kitsune})
+    yi_val_kitsune = pd.DataFrame(data={'ensemble': y_val_kitsune, 'prediction': y_pred_val_kitsune})
+    yi_test_kitsune = pd.DataFrame(data={'ensemble': y_test_kitsune, 'prediction': y_pred_test_kitsune})
+    
+    yi_train_ciciot = pd.DataFrame(data={'ensemble': y_train_ciciot, 'prediction': y_pred_train_ciciot})
+    yi_val_ciciot = pd.DataFrame(data={'ensemble': y_val_ciciot, 'prediction': y_pred_val_ciciot})
+    yi_test_ciciot = pd.DataFrame(data={'ensemble': y_test_ciciot, 'prediction': y_pred_test_ciciot})
+    
+    # plots
+    y_kdd = np.concatenate((y_train_kdd, y_val_kdd, y_test_kdd), axis=0)
+    y_nsl = np.concatenate((y_train_nsl, y_val_nsl, y_test_nsl), axis=0)
+    y_ids = np.concatenate((y_train_ids, y_val_ids, y_test_ids), axis=0)
+    y_kitsune = np.concatenate((y_train_kitsune, y_val_kitsune, y_test_kitsune), axis=0)
+    y_ciciot = np.concatenate((y_train_ciciot, y_val_ciciot, y_test_ciciot), axis=0)
+    
+    y_pred_kdd = np.concatenate((y_pred_train_kdd, y_pred_val_kdd, y_pred_test_kdd), axis=0)
+    y_pred_nsl = np.concatenate((y_pred_train_nsl, y_pred_val_nsl, y_pred_test_nsl), axis=0)
+    y_pred_ids = np.concatenate((y_pred_train_ids, y_pred_val_ids, y_pred_test_ids), axis=0)
+    y_pred_kitsune = np.concatenate((y_pred_train_kitsune, y_pred_val_kitsune, y_pred_test_kitsune), axis=0)
+    y_pred_ciciot = np.concatenate((y_pred_train_ciciot, y_pred_val_ciciot, y_pred_test_ciciot), axis=0)
+    
+    
+    yi_kdd = pd.DataFrame(data={'ensemble': y_kdd, 'prediction': y_pred_kdd})
+    yi_nsl = pd.DataFrame(data={'ensemble': y_nsl, 'prediction': y_pred_nsl})
+    yi_ids = pd.DataFrame(data={'ensemble': y_ids, 'prediction': y_pred_ids})
+    yi_kitsune = pd.DataFrame(data={'ensemble': y_kitsune, 'prediction': y_pred_kitsune})
+    yi_ciciot = pd.DataFrame(data={'ensemble': y_ciciot, 'prediction': y_pred_ciciot})
+    # lines
+    fig, axes = plt.subplots(5, 1, figsize=(9, 9), sharey=False)
+    n = 200
+    yi_kdd = yi_kdd.sample(n = n)
+    yi_nsl = yi_nsl.sample(n = n)
+    yi_ids = yi_ids.sample(n = n)
+    yi_kitsune = yi_kitsune.sample(n = n)
+    yi_ciciot = yi_ciciot.sample(n = n)
+    sns.lineplot(ax=axes[0], x=yi_kdd.index, y='ensemble', data=yi_kdd, color='blue')
+    sns.lineplot(ax=axes[0], x=yi_kdd.index, y='prediction', data=yi_kdd, color='red')
+    sns.lineplot(ax=axes[1], x=yi_nsl.index, y='ensemble', data=yi_nsl, color='blue')
+    sns.lineplot(ax=axes[1], x=yi_nsl.index, y='prediction', data=yi_nsl, color='red')
+    sns.lineplot(ax=axes[2], x=yi_ids.index, y='ensemble', data=yi_ids, color='blue')
+    sns.lineplot(ax=axes[2], x=yi_ids.index, y='prediction', data=yi_ids, color='red')
+    sns.lineplot(ax=axes[3], x=yi_kitsune.index, y='ensemble', data=yi_kitsune, color='blue')
+    sns.lineplot(ax=axes[3], x=yi_kitsune.index, y='prediction', data=yi_kitsune, color='red')
+    sns.lineplot(ax=axes[4], x=yi_ciciot.index, y='ensemble', data=yi_ciciot, color='blue')
+    sns.lineplot(ax=axes[4], x=yi_ciciot.index, y='prediction', data=yi_ciciot, color='red')
+    axes[0].set(xlabel=None, ylabel=None)
+    axes[1].set(xlabel=None, ylabel=None)
+    axes[2].set(xlabel=None, ylabel=None)
+    axes[3].set(xlabel=None, ylabel=None)
+    axes[4].set(xlabel=None, ylabel=None)
+    plt.savefig(filename+"_lines.png", dpi=300)  
+    
+    
 def get_data_description(size, metric, filename):
     
     # plots
